b09e0c5a69b9c54d5655b1e2980cd34932f4eb18
duzx16 <904663169@qq.com>
2023-06-27 14:40:44 +0800

    Add multi-gpu support

--- web_demo2.py
+++ web_demo2.py
@@ -14,10 +14,9 @@ st.set_page_config(
 def get_model():
     tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm2-6b", trust_remote_code=True)
     model = AutoModel.from_pretrained("THUDM/chatglm2-6b", trust_remote_code=True).cuda()
-    # 多显卡支持，使用下面三行代替上面两行，将num_gpus改为你实际的显卡数量
-    # model_path = "THUDM/chatglm2-6b"
-    # tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
-    # model = load_model_on_gpus(model_path, num_gpus=2)
+    # 多显卡支持，使用下面两行代替上面一行，将num_gpus改为你实际的显卡数量
+    # from utils import load_model_on_gpus
+    # model = load_model_on_gpus("THUDM/chatglm2-6b", num_gpus=2)
     model = model.eval()
     return tokenizer, model
 
