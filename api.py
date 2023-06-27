5916e3feebc11b671d471495a99b445d0caefac2
Qin Jianxiang <talkativedoggy@gmail.com>
2023-06-27 13:20:46 +0800

    add multiple GPUs usage comments for python files

--- api.py
+++ api.py
@@ -52,5 +52,9 @@ async def create_item(request: Request):
 if __name__ == '__main__':
     tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm2-6b", trust_remote_code=True)
     model = AutoModel.from_pretrained("THUDM/chatglm2-6b", trust_remote_code=True).cuda()
+    # 多显卡支持，使用下面三行代替上面两行，将num_gpus改为你实际的显卡数量
+    # model_path = "THUDM/chatglm2-6b"
+    # tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
+    # model = load_model_on_gpus(model_path, num_gpus=2)
     model.eval()
     uvicorn.run(app, host='0.0.0.0', port=8000, workers=1)
