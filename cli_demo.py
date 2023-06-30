e84a5f3c14eca59672fb38e7eceb686e1e0ea5d6
zxgov <64576649+zx2021@users.noreply.github.com>
2023-06-30 10:26:12 +0800

    Update cli_demo.py
    使用更先进的方式加载模型

--- cli_demo.py
+++ cli_demo.py
@@ -5,7 +5,7 @@ from transformers import AutoTokenizer, AutoModel
 import readline
 
 tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm2-6b", trust_remote_code=True)
-model = AutoModel.from_pretrained("THUDM/chatglm2-6b", trust_remote_code=True).cuda()
+model = AutoModel.from_pretrained("THUDM/chatglm2-6b", trust_remote_code=True, device='cuda')#.cuda()
 # 多显卡支持，使用下面两行代替上面一行，将num_gpus改为你实际的显卡数量
 # from utils import load_model_on_gpus
 # model = load_model_on_gpus("THUDM/chatglm2-6b", num_gpus=2)
