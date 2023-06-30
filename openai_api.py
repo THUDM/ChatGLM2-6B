3e3bd516d72d936b14a49f1429d23ba3040be336
zxgov <64576649+zx2021@users.noreply.github.com>
2023-06-30 10:22:27 +0800

    Update openai_api.py
    使用更先进的方式加载模型

--- openai_api.py
+++ openai_api.py
@@ -165,7 +165,7 @@ async def predict(query: str, history: List[List[str]], model_id: str):
 
 if __name__ == "__main__":
     tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm2-6b", trust_remote_code=True)
-    model = AutoModel.from_pretrained("THUDM/chatglm2-6b", trust_remote_code=True).cuda()
+    model = AutoModel.from_pretrained("THUDM/chatglm2-6b", trust_remote_code=True, device='cuda')#.cuda()
     # 多显卡支持，使用下面两行代替上面一行，将num_gpus改为你实际的显卡数量
     # from utils import load_model_on_gpus
     # model = load_model_on_gpus("THUDM/chatglm2-6b", num_gpus=2)
